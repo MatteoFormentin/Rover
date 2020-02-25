53375082b7471c9a531e82cd4ca7909f91fc1c49
Matteo Formentin <matteo.formentin@icloud.com>
2020-02-26 00:38:43 +0100

    Added compass correction.

--- file sviluppo/gps.dwl
+++
@@ -1,3 +0,0 @@
-matteo
-MacBook Pro di Matteo 
-Tuesday, 25 February 2020  01:23:39 Central European Standard Time
--- rover/compass.py
+++ rover/compass.py
@@ -1,10 +1,10 @@
 # LIBRARY: https://github.com/RigacciOrg/py-qmc5883l
 import py_qmc5883l
 
-
+CORRECTION = 0
 class Compass:
     def __init__(self):
         self.sensor = py_qmc5883l.QMC5883L()
 
     def getBearing(self):
-        return int(self.sensor.get_bearing())
+        return int((self.sensor.get_bearing() + CORRECTION ) % 360)
